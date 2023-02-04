450351f95402119f8e2b5975f62459e4d202624b
Takaki Wakuda <taka130102@hotmail.co.jp>
2023-02-04 21:01:42 +0900

    Add Name parameter

--- WindowsProgram/GetWindowsProgramCommand.cs
+++ WindowsProgram/GetWindowsProgramCommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Management.Automation;
+﻿using System.Collections.Generic;
+using System.Management.Automation;
 
 namespace WindowsProgram;
 
@@ -11,10 +12,59 @@ namespace WindowsProgram;
 public sealed class GetWindowsProgramCommand : PSCmdlet
 {
     /// <summary>
+    /// Gets or sets the Name parameter. Specifies the name of programs.
+    /// </summary>
+    [Parameter(Position = 0, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
+    [ValidateNotNullOrEmpty]
+    [SupportsWildcards]
+    public string[]? Name
+    {
+        get => _programNames;
+        set => _programNames = value;
+    }
+
+    private string[]? _programNames;
+    private readonly ProgramInfo[] _programInfos;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GetWindowsProgramCommand"/> class.
+    /// </summary>
+    public GetWindowsProgramCommand()
+    {
+        _programInfos = ProgramInfo.GetProgramInfos();
+    }
+
+    /// <summary>
     /// ProcessRecord override.
     /// </summary>
     protected override void ProcessRecord()
     {
-        WriteObject(ProgramInfo.GetProgramInfos(), true);
+        WriteObject(GetMatchingProgramInfos(), true);
+    }
+
+    private IEnumerable<ProgramInfo> GetMatchingProgramInfos()
+    {
+        if (_programNames is null)
+        {
+            return _programInfos;
+        }
+
+        HashSet<ProgramInfo> matchedProgramInfos = new();
+        WildcardPattern wildcard;
+
+        foreach (string pattern in _programNames)
+        {
+            wildcard = WildcardPattern.Get(pattern, WildcardOptions.IgnoreCase);
+
+            foreach (var programInfo in _programInfos)
+            {
+                if (wildcard.IsMatch(programInfo.Name))
+                {
+                    matchedProgramInfos.Add(programInfo);
+                }
+            }
+        }
+
+        return matchedProgramInfos;
     }
 }
--- test/Get-WindowsProgram.tests.ps1
+++ test/Get-WindowsProgram.tests.ps1
@@ -3,4 +3,15 @@
         $programs = Get-WindowsProgram
         $programs | Should -BeOfType "WindowsProgram.ProgramInfo"
     }
+
+    It "Gets installed programs whose name starts with 'Microsoft'" {
+        $programs = Get-WindowsProgram -Name "Microsoft*"
+        $programs | Should -BeOfType "WindowsProgram.ProgramInfo"
+        $programs.Name | Should -BeLike "Microsoft*"
+    }
+    It "Gets an installed program called 'Microsoft Edge'" {
+        $programs = Get-WindowsProgram -Name "Microsoft Edge"
+        $programs | Should -BeOfType "WindowsProgram.ProgramInfo"
+        $programs.Count | Should -Be 1
+    }
 }
